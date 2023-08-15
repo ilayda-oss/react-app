89873a5494d3d22e06dff9bf1ef3a7ab676ca4f5
sude ilayda ozkara <sudeozkara@marun.edu.tr>
2023-08-15 21:50:12 +0300

    1.

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from './Message'; // 'Message' bileşenini içe aktarın
+
+
+function App(){
+  return <div><Message/></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+// pascalCasing
+function Message() {
+    // JSX
+    const name = "İlayda";
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message; // Fonksiyonu parantez içine almadan sadece adını export ediyoruz.
